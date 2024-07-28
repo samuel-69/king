65c29b8d842cce97531b15550a3e84f952be4736
samuel-69 <161435346+samuel-69@users.noreply.github.com>
2024-07-28 17:52:09 +0300

    commiting files

--- structure/hello.js
+++ structure/hello.js
@@ -1,3 +1,21 @@
-let car = "missipi";
-for(let letter of car)
-    console.log(letter);
+const character="#";
+const count=10;
+const rows=[];
+let inverted=false;
+function sam(j,m){
+    return " ".repeat(m-9)+character.repeat(2*j-1)+" ".repeat(m-j);
+}
+for(let i=1;i<=count;i++){
+    if(inverted){
+        rows.unshift(sam(i,count));
+
+    }
+    else{
+        rows.push(sam(i,count));
+    }
+} 
+let result="";
+for(const row of rows){
+    result= result +"\n"+ row;
+}
+console.log(result);
